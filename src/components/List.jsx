2e9ebe4fa76d0fced38e7bdb1b00fd578a145c13
Edward White <edwardwhite@outlook.com>
2016-05-10 03:49:44 -0500

    react-skeleton completed

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+//test data,
+var ingredients = [{"id":1,"text":"ham"}, {"id":2,"text":"cheese"}, {"id":3,"text":"potatoes"}];
+var List = React.createClass({
+    render: function() {
+        var ListItems = ingredients.map(function(item) {
+          return <ListItem key={item.id} ingredient={item.text} />;
+        });
+        return (<ul>{ListItems}</ul>);
+    }
+});
+
+module.exports = List;
